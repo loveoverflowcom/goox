13c26e18e6338000ffdf9b8e365dcfcb2d07e24c
Pham Duc Manh <manhpd@bglobalcorp.com>
2026-03-24 16:05:13 +0700

    update gitignore

--- target/debug/deps/goox_core-b988691145d556a7.d
+++
@@ -1,7 +0,0 @@
-/Users/manhblue/Documents/personal/open_source/goox/target/debug/deps/goox_core-b988691145d556a7.d: crates/goox_core/src/lib.rs
-
-/Users/manhblue/Documents/personal/open_source/goox/target/debug/deps/libgoox_core-b988691145d556a7.rlib: crates/goox_core/src/lib.rs
-
-/Users/manhblue/Documents/personal/open_source/goox/target/debug/deps/libgoox_core-b988691145d556a7.rmeta: crates/goox_core/src/lib.rs
-
-crates/goox_core/src/lib.rs:
--- target/debug/deps/goox_core-d886d3baf23f3e1b.d
+++
@@ -1,5 +0,0 @@
-/Users/manhblue/Documents/personal/open_source/goox/target/debug/deps/goox_core-d886d3baf23f3e1b.d: crates/goox_core/src/lib.rs
-
-/Users/manhblue/Documents/personal/open_source/goox/target/debug/deps/libgoox_core-d886d3baf23f3e1b.rmeta: crates/goox_core/src/lib.rs
-
-crates/goox_core/src/lib.rs:
